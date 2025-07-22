411e5345f778fb5d2690b41d8e4e5a329dd4717b
Mohb Islam <mohabislam01@gmail.com>
2025-07-23 02:22:01 +0300

    Add Branches Controller and it's Views

---
+++ StudentAttendanceApp/Controllers/Branches.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentAttendanceApp.Data;
+using StudentAttendanceApp.Models;
+
+namespace StudentAttendanceApp.Controllers
+{
+    public class BranchesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public BranchesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var branches = _context.Branches.ToList();
+            return View(branches);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Branch branch)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Branches.Add(branch);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(branch);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var branch = _context.Branches.Find(id);
+            if (branch == null) return NotFound();
+            return View(branch);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Branch branch)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Branches.Update(branch);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(branch);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var branch = _context.Branches.Find(id);
+            if (branch == null) return NotFound();
+            return View(branch);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var branch = _context.Branches.Find(id);
+            if (branch == null) return NotFound();
+
+            _context.Branches.Remove(branch);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
