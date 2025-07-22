905f118553daff0e8e423502fcf9ca8def7e5475
Mohb Islam <mohabislam01@gmail.com>
2025-07-23 02:42:18 +0300

    Add Tracks Controller and it's Views And stop the ModelState for now for test

---
+++ StudentAttendanceApp/Controllers/Tracks.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using StudentAttendanceApp.Data;
+using StudentAttendanceApp.Models;
+
+namespace StudentAttendanceApp.Controllers
+{
+    public class TracksController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public TracksController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var tracks = _context.Tracks
+                                 .Select(t => new
+                                 {
+                                     t.Id,
+                                     t.Name,
+                                     t.CreatedAt,
+                                     BranchName = t.Branch.Name != null ? t.Branch.Name : "N/A"
+                                 })
+                                 .ToList();
+            ViewBag.Tracks = tracks;
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            ViewBag.Branches = new SelectList(_context.Branches, "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Track track)
+        {
+            //if (ModelState.IsValid)
+            //{
+                _context.Tracks.Add(track);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+    //        }
+            ViewBag.Branches = new SelectList(_context.Branches, "Id", "Name", track.BranchId);
+            return View(track);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var track = _context.Tracks.Find(id);
+            if (track == null) return NotFound();
+
+            ViewBag.Branches = new SelectList(_context.Branches, "Id", "Name", track.BranchId);
+            return View(track);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Track track)
+        {
+            //if (ModelState.IsValid)
+            //{
+                _context.Tracks.Update(track);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+       //     }
+            ViewBag.Branches = new SelectList(_context.Branches, "Id", "Name", track.BranchId);
+            return View(track);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var track = _context.Tracks
+                    .Include(t => t.Branch) //  تحميل الفرع المرتبط
+                    .FirstOrDefault(t => t.Id == id);
+            if (track == null) return NotFound();
+            return View(track);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var track = _context.Tracks
+                    .Include(t => t.Branch) //  تحميل الفرع المرتبط
+                    .FirstOrDefault(t => t.Id == id);
+            if (track == null) return NotFound();
+
+            _context.Tracks.Remove(track);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
