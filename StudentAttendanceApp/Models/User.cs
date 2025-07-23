592e1c79b8527d41b8bc538f29fa55da4dff3412
Mohb Islam <mohabislam01@gmail.com>
2025-07-23 18:10:14 +0300

    Add User Form View Model and Edit the user Model

--- StudentAttendanceApp/Models/User.cs
+++ StudentAttendanceApp/Models/User.cs
@@ -1,20 +1,30 @@
-﻿namespace StudentAttendanceApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentAttendanceApp.Models
 {
     public class User
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Full Name is required")]
         public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress]
         public string Email { get; set; }
-        public string PasswordHash { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
 
-        // علاقة مع الـ Role
+
+        [Required(ErrorMessage = "Password is required")]
+        public string PasswordHash { get; set; }
+
+        // Foreign Keys
+        [Required(ErrorMessage = "Role is required")]
         public int RoleId { get; set; }
         public Role Role { get; set; }
 
-        // علاقة مع الـ Branch
+        [Required(ErrorMessage = "Branch is required")]
         public int BranchId { get; set; }
         public Branch Branch { get; set; }
     }
-
 }
---
+++ StudentAttendanceApp/ViewModels/UserFormViewModel.cs
@@ -0,0 +1,12 @@
+﻿using StudentAttendanceApp.Models;
+
+namespace StudentAttendanceApp.ViewModels
+{
+    public class UserFormViewModel
+    {
+        public User User { get; set; }
+        public List<Role> Roles { get; set; }
+        public List<Branch> Branches { get; set; }
+        public bool IsEditMode => User.Id != 0;
+    }
+}
