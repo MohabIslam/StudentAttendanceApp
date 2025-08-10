5a1ac50c0bdc5284314de00c7c1bd50d3beeb874
Mohb Islam <mohabislam01@gmail.com>
2025-08-10 19:53:50 +0300

    Remove Roles and add User Model

--- StudentAttendanceApp/Models/User.cs
+++ StudentAttendanceApp/Models/User.cs
@@ -1,30 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace StudentAttendanceApp.Models
 {
-    public class User
+    public class User : IdentityUser
     {
-        public int Id { get; set; }
-
         [Required(ErrorMessage = "Full Name is required")]
         public string FullName { get; set; }
 
-        [Required(ErrorMessage = "Email is required")]
-        [EmailAddress]
-        public string Email { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
 
-
-        [Required(ErrorMessage = "Password is required")]
-        public string PasswordHash { get; set; }
-
-        // Foreign Keys
-        [Required(ErrorMessage = "Role is required")]
-        public int RoleId { get; set; }
-        public Role Role { get; set; }
-
+        // Foreign Key to Branch
         [Required(ErrorMessage = "Branch is required")]
         public int BranchId { get; set; }
         public Branch Branch { get; set; }
     }
 }
+    
